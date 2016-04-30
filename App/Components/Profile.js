05395bab0b05873e53e22a0f3d3085a0df61380e
Aki Suzuki <aki@siliconsurfer.io>
2016-04-30 00:04:23 -0700

    separator helper

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -1,5 +1,7 @@
 var React = require('react-native');
 var Badge = require('./Badge.js');
+var Separator = require('./Helpers/Separator.js');
+
 
 var {
   Text,
@@ -49,6 +51,7 @@ class Profile extends React.Component {
               <Text style={styles.rowTitle}> {this.getRowTitle(userInfo,item)} </Text>
               <Text style={styles.rowContent}> {userInfo[item]} </Text>
             </View>
+            <Separator />
           </View>
         )
       }
